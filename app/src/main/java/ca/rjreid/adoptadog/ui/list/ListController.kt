1a08ba04689f7ee094829151a9f182b8a2e7fed2
Reid <rreid112@gmail.com>
2017-09-30 09:48:33 -0600

    Implemented a refresh layout

--- app/src/main/java/ca/rjreid/adoptadog/ui/list/ListController.kt
+++ app/src/main/java/ca/rjreid/adoptadog/ui/list/ListController.kt
@@ -16,14 +16,18 @@ import ca.rjreid.adoptadog.ui.dogdetails.DogDetailsActivity
 import com.bluelinelabs.conductor.Controller
 
 
-class ListController : Controller() {
-
+class ListController : Controller(), SwipeRefreshLayout.OnRefreshListener {
+    //region Variables/Views
     private var dm: DataManager? = null
-    private var recyclerView: RecyclerView? = null
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private lateinit var recyclerView: RecyclerView
     private var adapter = ListAdapter({ dog -> dogClick(dog) })
+    //endregion
 
+    //region Lifecycle
     override fun onCreateView(@NonNull inflater: LayoutInflater, @NonNull container: ViewGroup): View {
         val view = inflater.inflate(R.layout.controller_list, container, false)
+        swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout)
         recyclerView = view.findViewById(R.id.recyclerView)
         return view
     }
@@ -31,26 +35,47 @@ class ListController : Controller() {
     override fun onAttach(view: View) {
         super.onAttach(view)
         val layoutManager = GridLayoutManager(activity, 2)
-        recyclerView?.layoutManager = layoutManager
-        recyclerView?.adapter = adapter
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
+
+        swipeRefreshLayout.setOnRefreshListener(this)
+
         refresh()
     }
 
+    override fun onDetach(view: View) {
+        super.onDetach(view)
+        swipeRefreshLayout.setOnRefreshListener(null)
+    }
+    //endregion
+
+    //region Setters
     fun setDataManager(dm: DataManager) {
         this.dm = dm
     }
+    //endregion
 
+    //region Refresh Layout Listener Implementation
+    override fun onRefresh() {
+        refresh()
+    }
+    //endregion
+
+    //region Helpers
     private fun refresh() {
+        swipeRefreshLayout.isRefreshing = true
         dm?.fetchDogs()?.subscribe(
                 { updateDogs(it) },
                 { showError() })
     }
 
     private fun updateDogs(dogs: List<Dog>) {
+        swipeRefreshLayout.isRefreshing = false
         adapter.populateDogs(dogs)
     }
 
     private fun showError() {
+        swipeRefreshLayout.isRefreshing = false
         Toast.makeText(activity, "Oops, something has gone wrong :(", Toast.LENGTH_LONG).show()
     }
 
@@ -59,5 +84,5 @@ class ListController : Controller() {
             startActivity(DogDetailsActivity.createIntent(it, dog))
         }
     }
-
+    //endregion
 }
