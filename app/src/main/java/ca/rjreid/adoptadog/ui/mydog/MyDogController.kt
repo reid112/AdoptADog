6dc5f35139595109b6a59dac1b6e7169c7dee86e
Reid <rreid112@gmail.com>
2017-09-30 10:04:42 -0600

    Added ui for my dog

--- app/src/main/java/ca/rjreid/adoptadog/ui/mydog/MyDogController.kt
+++ app/src/main/java/ca/rjreid/adoptadog/ui/mydog/MyDogController.kt
@@ -5,13 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ca.rjreid.adoptadog.R
+import ca.rjreid.adoptadog.util.image
 import com.bluelinelabs.conductor.Controller
+import de.hdodenhof.circleimageview.CircleImageView
 
 class MyDogController : Controller() {
 
+    private lateinit var dogImage: CircleImageView
+
     override fun onCreateView(@NonNull inflater: LayoutInflater, @NonNull container: ViewGroup): View {
         val view = inflater.inflate(R.layout.controller_my_dog, container, false)
+        dogImage = view.findViewById(R.id.dogImage)
         return view
     }
 
+    override fun onAttach(view: View) {
+        super.onAttach(view)
+        dogImage.image("http://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg")
+    }
 }
